6a3cf96a6af6244d7b8faa89c43ead4ad34b811c
Afmarnob <afmarnob@gmail.com>
2026-08-09 14:11:00 +0600

    create doctor schedule

--- src/app/modules/doctorSchedule/doctorSchedule.service.ts
+++ src/app/modules/doctorSchedule/doctorSchedule.service.ts
@@ -1,12 +1,33 @@
 import { Request, Response } from "express";
 import catchAsync from "../../shared/catchAsync";
+import { prisma } from "../../shared/prisma";
 
-const insertIntoDB = async (user: any, payload: any) => {
-  console.log("user", user);
-  console.log("payload", payload);
+const insertIntoDB = async (
+  user: any,
+  payload: {
+    scheduleIds: string[];
+  },
+) => {
+  const doctorData = await prisma.doctor.findUniqueOrThrow({
+    where: {
+      email: user.email,
+      userId: user.userId,
+    },
+  });
+
+  const doctorScheduleData = payload.scheduleIds.map((scheduleId) => ({
+    doctorId: doctorData.id,
+    scheduleId,
+  }));
+  return await prisma.doctorSchedule.createMany({
+    data: doctorScheduleData,
+    skipDuplicates: true,
+  });
+  console.log(doctorScheduleData, "doctorScheduleData");
   return {
     user,
     payload,
+    doctorScheduleData,
   };
 };
 
